3336da1460d32ce216beb5c3808ba9467a45a0ce
JulioLeitao <juliocesarbl@gmail.com>
2016-06-28 15:12:43 -0300

    Initial commit

---
+++ app/src/main/java/com/julioleitao/wearnotificationdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.julioleitao.wearnotificationdemo;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent i = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0, i, 0 );
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(android.R.drawable.arrow_up_float)
+                .setContentTitle("Notification Title")
+                .setContentText("Hi Julio, How's it going?")
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(0,notificationBuilder.build());
+    }
+}
